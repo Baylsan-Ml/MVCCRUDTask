b9de53ab0c69cbfc516e858bf91aad1acdcc4e64
Baylsan-ML <bailsanmletat99@gmail.com>
2026-05-22 15:54:04 +0300

    Add project files.

---
+++ CRUDTask/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using CRUDTask.Data;
+using CRUDTask.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDTask.Controllers
+{
+    public class UsersController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var user = context.Users.ToList();
+            return View("Index", user);
+        }
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        public IActionResult Store(User Request)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Users.Add(Request);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+        public IActionResult Details(int id)
+        {
+            var user = context.Users.Find(id);
+            return View("Details", user);
+        }
+        public IActionResult Edit(int id) {
+            var user = context.Users.Find(id); 
+            return View("Edit", user);
+        }
+        public IActionResult Update(User Request)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Users.Update(Request);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Edit", Request);
+        }
+        public IActionResult Delete(int id)
+        {
+            var user = context.Users.Find(id);
+            context.Users.Remove(user);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUDTask/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUDTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CRUDTask.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=CRUDTask;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
