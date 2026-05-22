569cb1b5d3a155e7f0053afd0e5997dca3cc4351
Baylsan-Ml <bailsanmletat99@gmail.com>
2026-05-22 17:58:39 +0300

    auth layout

---
+++ CRUDTask/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDTask.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+    }
+}
