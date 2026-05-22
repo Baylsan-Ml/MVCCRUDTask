7685be952a3a8bf8fd763bd03ca982dc1161a12b
Baylsan-Ml <bailsanmletat99@gmail.com>
2026-05-22 18:41:51 +0300

    fixing anotaion

--- CRUDTask/Models/User.cs
+++ CRUDTask/Models/User.cs
@@ -11,7 +11,6 @@ namespace CRUDTask.Models
         public string Name { get; set; }
         [Range(15,35,ErrorMessage ="Age must be between 15 and 35")]
         public int Age { get; set; }
-        [Display(Name ="Email Address",Prompt ="Enter your email address here...")]
         [Required(ErrorMessage ="City is required")]
         [MinLength(2,ErrorMessage ="Length must be at least 2 characters")]
         [MaxLength(50,ErrorMessage ="Length must be at most 50 characters")]
